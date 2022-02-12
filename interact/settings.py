a50463dd1fc122f0509f8bec57fcce76d6bad221
Mac Maxwell <macsakini@hotmail.com>
2022-02-12 12:45:31 +0300

    Update settings.py

--- interact/settings.py
+++ interact/settings.py
@@ -127,7 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # this is the static files folder name which you created in django project root folder. This is different from above STATIC_URL. 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'interact/static'),
+    os.path.join(BASE_DIR, 'interacter/static'),
 ]
 
 STATICFILES_FINDERS = (
