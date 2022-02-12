ace105d394302254735d1eed391e7ad8b494dce4
Pinky-alt-spec <pinkymononyane@gmail.com>
2022-02-12 03:34:42 +0300

    first commit

--- interact/settings.py
+++ interact/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'https://rarelane.azurewebsites.net/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # this is the static files folder name which you created in django project root folder. This is different from above STATIC_URL. 
 STATICFILES_DIRS = [
